cafb149e0f80c752d4678b7a87df35741777fb9d
Jimmy W. Hough Jr <jimmyhoughjr@me.com>
2024-08-07 17:52:31 -0500

    wip

--- generate.sh
+++ generate.sh
@@ -1,8 +1,18 @@
-##!/bin/sh
+##!/bin/bash
 # This is a comment!
-echo Hello World   
-vapor new test --template https://github.com/jhoughjr/vapor-auth-template
-curl 'https://mkjwk.org/jwk/rsa?alg=RS256&use=sig&gen=sha256&size=2048' \
-  -o test/keypair.jwks
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+echo Hello World
+vapor new $1 --template https://github.com/jhoughjr/vapor-auth-template
+echo "${GREEN}Generating new jwks from mkjwk.org ...${NC}"
+curl -s 'https://mkjwk.org/jwk/rsa?alg=RS256&use=sig&gen=sha256&size=2048' \
+  -o $1/keypair.jwks
+echo "${YELLOW}Done.${NC}"
+echo "${GREEN}Starting development server...  ${NC}"
+cd ./$1/
+swift run App serve --env development
  
   
