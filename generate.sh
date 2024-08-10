5947a0a61fa0d21df14fc941fd53e1a765a4a2ff
Jimmy W. Hough Jr <jimmyhoughjr@me.com>
2024-08-10 09:18:55 -0500

    dead is better

--- generate.sh
+++ generate.sh
@@ -8,13 +8,14 @@ NC='\033[0m' # No Color
 echo Hello World
 sudo apt install jq
 vapor new $1 --template https://github.com/jhoughjr/vapor-auth-template.git
+cd ./$1/
 echo "${GREEN}Generating new jwks from mkjwk.org ...${NC}"
  curl -s 'https://mkjwk.org/jwk/rsa?alg=RS256&use=sig&gen=sha256&size=2048' \
  | jq ."jwks" >> ./keypair.jwks
 
 echo "${YELLOW}Done.${NC}"
 echo "${GREEN}Starting development server...  ${NC}"
-cd ./$1/
+
 swift run App serve --env development
  
   
