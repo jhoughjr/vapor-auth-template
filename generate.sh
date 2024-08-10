a3380d72f2887b48c76d1873728d0ffcae41383e
Jimmy W. Hough Jr <jimmyhoughjr@me.com>
2024-08-10 09:12:23 -0500

    most useless

--- generate.sh
+++ generate.sh
@@ -8,8 +8,9 @@ NC='\033[0m' # No Color
 echo Hello World
 vapor new $1 --template https://github.com/jhoughjr/vapor-auth-template.git
 echo "${GREEN}Generating new jwks from mkjwk.org ...${NC}"
-curl -s 'https://mkjwk.org/jwk/rsa?alg=RS256&use=sig&gen=sha256&size=2048' \
-  -o $1/keypair.jwks
+ curl -s 'https://mkjwk.org/jwk/rsa?alg=RS256&use=sig&gen=sha256&size=2048' \
+ | jq ."jwks" >> ./keypair.jwks
+
 echo "${YELLOW}Done.${NC}"
 echo "${GREEN}Starting development server...  ${NC}"
 cd ./$1/
