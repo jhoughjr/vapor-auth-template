b8c86bbcc9ef32a193b8d5bd6f2363eb014c6f27
Jimmy W. Hough Jr <jimmyhoughjr@me.com>
2024-08-11 14:00:48 -0500

    mac/linux work

--- generate.sh
+++ generate.sh
@@ -5,13 +5,40 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
-REQUIRED_PKG="jq"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-  sudo apt-get --yes install $REQUIRED_PKG
+
+  
+brew_install() {
+    echo "\nInstalling $1 if not installed..."
+    if brew list $1 &>/dev/null; then
+        echo "${1} is already installed"
+    else
+        brew install $1 && echo "$1 is installed"
+    fi
+}
+
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform
+    brew_install jq
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+    echo use apt
+    REQUIRED_PKG="jq"
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+    echo Checking for $REQUIRED_PKG: $PKG_OK
+    if [ "" = "$PKG_OK" ]; then
+    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+    sudo apt-get --yes install $REQUIRED_PKG
+    fi
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under 32 bits Windows NT platform
+    echo win 32
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under 64 bits Windows NT platform
+    echo win 64
 fi
+
+
 echo Generating project from template....
 vapor new $1 --template https://github.com/jhoughjr/vapor-auth-template.git
 cd ./$1/
@@ -21,4 +48,3 @@ echo "${GREEN}Generating new jwks from mkjwk.org ...${NC}"
 
 echo "${YELLOW}Done.${NC}"
  
-  
