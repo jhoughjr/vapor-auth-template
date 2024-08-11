4ebfc461e9fe892f20fafba7c03ccaeb3a89ec0e
Jimmy W. Hough Jr <jimmyhoughjr@me.com>
2024-08-11 12:34:41 -0500

    better generate script

--- generate.sh
+++ generate.sh
@@ -5,8 +5,14 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
-echo Hello World
-sudo apt install jq
+REQUIRED_PKG="jq"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
+echo Generating project from template....
 vapor new $1 --template https://github.com/jhoughjr/vapor-auth-template.git
 cd ./$1/
 echo "${GREEN}Generating new jwks from mkjwk.org ...${NC}"
